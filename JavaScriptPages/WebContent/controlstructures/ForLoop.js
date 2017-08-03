0784547723e3a1113a59d1a254adf12d416c04f1
arun789 <adwiti1975@gmail.com>
2017-08-02 20:17:52 -0700

    for loop

---
+++ JavaScriptPages/WebContent/controlstructures/ForLoop.js
@@ -0,0 +1,12 @@
+//Iterate over a loop with an interval of 2
+for (var i = 0; i <= 10; i = i + 2) {
+	console.log(i);
+}
+
+// Nested for loop
+for (var i = 0; i < 10; i++) {
+	console.log("i = " + i);
+	for (var j = 0; j < 15; j++) {
+		console.log("j = " + j)
+	}
+}
