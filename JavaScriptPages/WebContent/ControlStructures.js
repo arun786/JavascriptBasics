1978e4003d8369f3b5ff027b589754d73cb62e64
arun789 <adwiti1975@gmail.com>
2017-08-02 19:46:58 -0700

    control structure

---
+++ JavaScriptPages/WebContent/ControlStructures.js
@@ -0,0 +1,22 @@
+var myName = "arun";
+console.log(myName);
+
+var myAge = 19;
+
+if (myAge > 18) {
+	console.log("I am an adult");
+} else {
+	console.log("I am not an adult");
+}
+
+var myAge = 19;
+
+if (myAge == 2) {
+	console.log("I am a kid");
+} else if (myAge > 2 && myAge < 10) {
+	console.log("I am a child");
+} else if (myAge >= 10 && myAge < 20) {
+	console.log("I am a teen");
+} else {
+	console.log("I am an adult");
+}
